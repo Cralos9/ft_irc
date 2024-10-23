b4940464c8d543bb52be567ac51aa5c2925ff14c
joaonunesss <joaom_braga@hotmail.com>
2024-10-23 14:37:57 +0100

    Who Command

--- src/commands/Who.cpp
+++ src/commands/Who.cpp
@@ -3,15 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   Who.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmarinho <jmarinho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:19:35 by rumachad          #+#    #+#             */
-/*   Updated: 2024/10/21 15:11:14 by rumachad         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:39:46 by jmarinho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Commands.hpp"
 
+// https://datatracker.ietf.org/doc/html/rfc2812#section-3.6.1
+
+/* Command: WHO
+   Parameters: [ <mask> [ "o" ] ]
+
+   The WHO command is used by a client to generate a query which returns
+   a list of information which 'matches' the <mask> parameter given by
+   the client.  In the absence of the <mask> parameter, all visible
+   (users who aren't invisible (user mode +i) and who don't have a
+   common channel with the requesting client) are listed.  The same
+   result can be achieved by using a <mask> of "0" or any wildcard which
+   will end up matching every visible user.
+
+   The <mask> passed to WHO is matched against users' host, server, real
+   name and nickname if the channel <mask> cannot be found. */
+   
 Who::Who(Server &server) : ACommand(server)
 {
 /* 	std::cout << "Who constructor" << std::endl; */
@@ -24,14 +40,14 @@ Who::~Who()
 
 int Who::run()
 {
-	User *user = NULL;
-
-	user = _server.get_user(_args[0]);
-	if (user == NULL)
+	if (_args[0].empty()) //If mask empty list everyone in server
 	{
-		std::cout << "User does't exists" << std::endl;
-		return (1);
+		for (std::map<int, User>::iterator *it; it != _clients.end(); ++it)
+		{
+			_server.send_msg_all_users(server._clients, _clients, CHSELF);
+		}
 	}
-	
-	return (0);
+	// If the mask is a channel, list everyone in the channel
+	// If the mask is a user, list the user information
+	return (EXIT_SUCCESS);
 }
