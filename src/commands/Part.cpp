9924423ef6d630fe061fdab4cea4c03eb2696cae
rmsa42 <rui.soares.almeida@outlook.pt>
2024-11-21 18:21:45 +0000

    Fixed a sigsev in Part command

--- src/commands/Part.cpp
+++ src/commands/Part.cpp
@@ -6,7 +6,7 @@
 /*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 13:38:31 by cacarval          #+#    #+#             */
-/*   Updated: 2024/11/21 15:40:52 by rumachad         ###   ########.fr       */
+/*   Updated: 2024/11/21 18:22:23 by rumachad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,9 @@ const std::vector<std::string> split(const std::string &targets)
 int Part::run()
 {
 	const std::vector<std::string> &targets = split(_args[0]);
-	std::deque<std::string> params(1);
+	std::deque<std::string> params(2);
 
+	params[1] = _args[1];
 	for (std::vector<std::string>::const_iterator it = targets.begin(); it != targets.end(); it++)
 	{
 		params[0] = *it;
@@ -45,14 +46,14 @@ int Part::run()
 		if (channel == NULL)
 		{
 			_server.send_numeric(*_user, ERR_NOSUCHCHANNEL, "%s :No such channel",
-									*it->c_str());
+									it->c_str());
 			return (1);
 		}
 
 		if (!channel->is_user_on_ch(*_user))
 		{
 			_server.send_numeric(*_user, ERR_NOTONCHANNEL, "%s :You're not on that channel",
-									*it->c_str());
+									it->c_str());
 			return (1);
 		}
 		_user->make_msg("PART", params);
