10befd4bd6af07391b69c810e7f061f512e95266
joaonunesss <joaom_braga@hotmail.com>
2024-11-21 15:02:28 +0000

    privmsg aceita multi mas envia mensagens duplicadas

--- src/commands/PrivMsg.cpp
+++ src/commands/PrivMsg.cpp
@@ -24,32 +24,48 @@ PrivMsg::~PrivMsg()
 
 int PrivMsg::run()
 {
-	Channel *ch = NULL;
-	User *receiver = NULL;
+	Channel	*ch = NULL;
+	User	*receiver = NULL;
 
-	if (this->_args[0].find('#') != std::string::npos)
+	std::string target;
+	std::istringstream iss(_args[0]);
+
+	if (_args[1].find(':') != std::string::npos)
+		_args[1].erase(0, 1);
+
+	while (std::getline(iss, target, ','))
 	{
-		ch = _server.check_channel(_args[0]);
-		if (ch == NULL)
+		if (target.find('#') != std::string::npos)
 		{
-			_server.send_numeric(*_user, ERR_NOSUCHCHANNEL, "%s :No such channel",
-								_args[0].c_str());
-			return (1);
+			ch = _server.check_channel(target);
+			if (ch == NULL)
+			{
+				_server.send_numeric(*_user, ERR_NOSUCHCHANNEL, "%s :No such channel",
+									target.c_str());
+				return (EXIT_FAILURE);
+			}
+			std::cout << "target in channel " << target << " msg " << _args[1] << std::endl;
+
+			_user->set_buffer(":" + _user->get_nick() + "!" + _user->get_username() + "@" + _user->get_hostname() + " " + "PRIVMSG " + target + " :" + _args[1] + "\r\n");
+			_server.send_msg_to_channel(*ch, *_user, CHOTHER);
+			ch = NULL;
 		}
-		_user->make_msg("PRIVMSG", _args);
-		_server.send_msg_to_channel(*ch, *_user, CHOTHER); 
-	}
-	else
-	{
-		receiver = this->_server.get_user(this->_args[0]);
-		if (receiver == NULL)
+		else
 		{
-			_server.send_numeric(*_user, ERR_NOSUCHNICK, "%s :No such nick",
-									_args[0].c_str());
-			return (1);
+			receiver = this->_server.get_user(target);
+			if (receiver == NULL)
+			{
+				_server.send_numeric(*_user, ERR_NOSUCHNICK, "%s :No such nick",
+										target.c_str());
+				return (EXIT_FAILURE);
+			}
+			std::cout << "target in user " << target << " msg " << _args[1] << std::endl;
+
+			_user->set_buffer(":" + _user->get_nick() + "!" + _user->get_username() + "@" + _user->get_hostname() + " " + "PRIVMSG " + target + " :" + _args[1] + "\r\n");
+			_server.send_msg_one_user(receiver->get_fd(), *_user);
+			receiver = NULL;
 		}
-		_user->make_msg("PRIVMSG", _args);
-		_server.send_msg_one_user(receiver->get_fd(), *_user);
+		target.clear();
 	}
-	return (0);
+	return (EXIT_SUCCESS);
 }
