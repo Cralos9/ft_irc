3de67863999449808105f2e4396d9e17b21569bf
Cralos9 <carlosc.rocha77@gmail.com>
2024-11-26 13:58:40 +0000

    Quit on CTRL+C

--- src/commands/Quit.cpp
+++ src/commands/Quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rumachad <rumachad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cacarval <cacarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:49:40 by cacarval          #+#    #+#             */
-/*   Updated: 2024/11/25 14:35:31 by rumachad         ###   ########.fr       */
+/*   Updated: 2024/11/26 13:54:53 by cacarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,13 @@ int Quit::run()
 	if (_args.empty())
 		_args.push_back(":Client Quit");
 	_user->make_msg("QUIT", _args);
-	_server.send_msg_all_users(*this->_user);
+	std::string ch;
+	std::istringstream iss(_server.channels_user_joined(*_user));
+	while (std::getline(iss, ch, ' '))
+	{
+		if (!ch.empty())
+			_server.send_msg_to_channel(*_server.check_channel(ch), *this->_user, CHOTHER);
+	}
 	_server.disconnect_user(*this->_user);
 	return (1);
 }
